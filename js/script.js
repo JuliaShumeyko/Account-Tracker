67c8fb4673e8da8888b0d3f6dc66955038899f70
Julia Shumeyko <funkydinosaur@Julias-MacBook-Air.local>
2019-04-09 13:49:14 +0300

    fixed delete function, added styles

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
+
+// ************  DECLARING VARIABLE **************
+
 const descInput = document.querySelector('.description-input');
 const amountInput = document.querySelector('.amount-input');
 const addBtn = document.querySelector('button');
 const clearBtn = document.querySelector('.clear-button');
 let accBalance = 0;
-let transactions = [['Sold house', 200000, '22/05/1988'],['Sold car', 7000, '28/11/1985'], ['Salary', 748000, '31/12/1999'], ['Bought a boat', -980000, '01/06/2000']];
+let transactions = [];
 
+// ************  FUNCTIONS **************
 
+// Adding array to Local Storage
 function arrayToStorage(arr) {
     let arrJSON = JSON.stringify(arr, undefined, 2);
     localStorage.setItem('transactions', arrJSON);
 }
 
+// Getting array from Local Storage
 function getFromStorage() {
-    if (localStorage.getItem('transaction')) transactions = JSON.parse(localStorage.getItem('transactions'))
-    else transactions = [['Sold house', 200000, '22/05/1988'],['Sold car', 7000, '28/11/1985'], ['Salary', 748000, '31/12/1999'], ['Bought a boat', -980000, '01/06/2000']];
+    if (localStorage.getItem('transactions')) transactions = JSON.parse(localStorage.getItem('transactions'))
+    else transactions = [];
 }
 
+// Creating a transaction array
 function createTransaction() {
+    if (descInput.value != '' && amountInput.value != 0) {
     let checkbox = document.querySelector('.onoffswitch-checkbox');
     let transaction;
     if (!checkbox.checked) {
@@ -26,37 +34,56 @@ function createTransaction() {
     };  
     transactions.push(transaction);
     arrayToStorage(transactions);
+} else {
+    alert('Please, enter the transaction description and amount!');
+}
 }
 
-getFromStorage();
-renderTransactions(transactions);
-
+// Rendering the transactions
 function renderTransactions(transactions) {
 
+
+// Creating the account balance interface
+
     document.querySelector('.balance-container').innerHTML = '';
-    let balanceInfo = document.createElement('h1');
-    balanceInfo.innerHTML = 'Account Balance: ' + (calcBalance()).toLocaleString() + ' €';
+    let balanceInfo = document.createElement('div');
+    balanceInfo.className = 'balance';
+    let balanceAmount = (calcBalance()).toLocaleString();
+    if (calcBalance() >= 0) {
+        balanceInfo.innerHTML = `Account Balance: <span class="positive">${balanceAmount}</span> €`;
+    } else {
+        balanceInfo.innerHTML = `Account Balance: <span class="negative">${balanceAmount}</span> €`;
+    };
     document.querySelector('.balance-container').appendChild(balanceInfo);
-    transactions.forEach(function(transaction) {
+
+
+
+// Creating transaction boxes
+
+    transactions.forEach(function(transaction, index) {
         let transactionBox = document.createElement('div');
-        transactionBox.className = 'transaction';
+        transactionBox.id = index;
+        transactionBox.className = 'transaction ';
         if (transaction[1] > 0) {
             transactionBox.innerHTML = `
             <div class="trans-description">${transaction[0]}</div>
             <div class="trans-date">${transaction[2]}</div>
-            <div class="trans-amount green">${transaction[1].toLocaleString()} €</div>`;
-
+            <div class="trans-amount green">${transaction[1].toLocaleString()} €</div>
+            <i class="far fa-trash-alt delete-icon"></i>`;
         }
         else {
         transactionBox.innerHTML = `
                                     <div class="trans-description">${transaction[0]}</div>
                                     <div class="trans-date">${transaction[2]}</div>
-                                    <div class="trans-amount">${transaction[1].toLocaleString()} €</div>`};
-        document.querySelector('.balance-container').appendChild(transactionBox);
-
-    })
+                                    <div class="trans-amount">${transaction[1].toLocaleString()} €</div>
+                                    <i class="far fa-trash-alt delete-icon"></i>`};
+        
+    document.querySelector('.balance-container').appendChild(transactionBox);    
+})
 }
 
+
+// Getting the current date
 function currentDate() {
     var currentDate = new Date();
     day = "00" + currentDate.getDate();
@@ -68,6 +95,7 @@ function currentDate() {
     return `${day}/${month}/${year}`
 }
 
+// Calculating the account balance
 function calcBalance() {
     let balanceSum = 0;
     transactions.forEach(function(transaction) {
@@ -76,17 +104,54 @@ function calcBalance() {
     return balanceSum;
 }
 
-addBtn.addEventListener('click', e => {
-    createTransaction();
-    renderTransactions(transactions);
-})
-
+// Clearing Local Storage
 function clearStorage() {
     localStorage.clear();
     getFromStorage();
     renderTransactions(transactions);
 }
 
+// Deleting a transaction
+function deleteElementFromArray(arr, index) {
+    arr.splice(index, 1);
+    arrayToStorage(arr);
+    renderTransactions(transactions);
+    assignEventListenerToDeleteIcons();
+}
+
+// Assigning an event listener to Delete Icons
+function assignEventListenerToDeleteIcons(){
+
+    let deleteIcon = document.querySelectorAll('.delete-icon');
+    deleteIcon.forEach(function(el){
+            el.addEventListener('click', e => {
+                deleteElementFromArray(transactions, e.currentTarget.parentNode.id);
+            });
+    });    
+
+}
+// Initializition
+function init() {
+    getFromStorage();
+    renderTransactions(transactions);
+    }
+
+
+// ************  EVENT LISTENERS **************
+
 clearBtn.addEventListener('click', e => {
     clearStorage();
 })
+
+addBtn.addEventListener('click', e => {
+    createTransaction();
+    renderTransactions(transactions);
+    assignEventListenerToDeleteIcons();
+})
+
+
+
+// ************  EXECUTING **************
+
+
+init();
